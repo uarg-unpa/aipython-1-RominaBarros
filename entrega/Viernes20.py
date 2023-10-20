cf10ac48361e93eb96a96c9048149a8ad6111f57
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-20 21:34:55 +0000

    viernes

---
+++ entrega/Viernes20.py
@@ -0,0 +1,11 @@
+print("viernes en AIPYTHON")#invocacion de la funcion("los argumentos")
+print ()#imprime linea en blanco
+#print (" )#error
+print("taller", "AIPython", "viernes")  
+print("primer argumento", "segundo argumento",sep="_")# sep ="_"   = me daje un guion bajo en el medio de los doa argumentos
+print("primer argumento", "segundo argumento",end="\n")# end="\n" = me deja un espacio entre los dos argumentos
+print ("R","O","M", "Y ",end=" * ")
+#print ("la funcion\"print\"...")"\n"
+print ('la funcion "print"...'"\t")# si comienza con comilla ''simple, termina con las mismas
+print ('la funcion print\t hola')# si comienza con comilla dobre, termina con comilla doble
+#claer = para limpiar
