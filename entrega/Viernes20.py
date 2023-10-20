89a820ab83abcd93abbc4033c4a7d72f1f1bf7b0
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-20 22:00:11 +0000

    viernes

--- entrega/Viernes20.py
+++ entrega/Viernes20.py
@@ -9,3 +9,8 @@ print ("R","O","M", "Y ",end=" * ")
 print ('la funcion "print"...'"\t")# si comienza con comilla ''simple, termina con las mismas
 print ('la funcion print\t hola')# si comienza con comilla dobre, termina con comilla doble
 #claer = para limpiar
+print(23)
+print(23.3)
+print(True)
+print("hola", "mi edad es:",40 )
+#print("hola", "mi edad es:" 40 ) como falta la ultima coma, antes del numero entero, da error
--- entrega/test.py
+++ entrega/test.py
@@ -1,2 +1,6 @@
-print ("LAS MAQUINAS ME SORPRENDEN CON MUCHA")
+print ("LAS MAQUINAS ME SORPRENDEN CON MUCHA FRECUENCIA")
 print ()
+print("hoy es viernes :)")
+print ("23", '"23"')
+print ('una computadora puede ser llamada "inteligente" si logra engañar a una personahacie{endole} haciéndole creer que
+es un humano.')
