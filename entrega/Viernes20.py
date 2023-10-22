d686f9ba3b4ede898eb20a7153dd89309a03c2ab
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-22 22:41:12 +0000

    1 y 2 casi completos

--- entrega/Viernes20.py
+++ entrega/Viernes20.py
@@ -1,11 +1,11 @@
 print("viernes en AIPYTHON")#invocacion de la funcion("los argumentos")
 print ()#imprime linea en blanco
 #print (" )#error
-print("taller", "AIPython", "viernes")  
-print("primer argumento", "segundo argumento",sep="_")# sep ="_"   = me daje un guion bajo en el medio de los doa argumentos
+print("taller", "AIPython", "viernes")  #TRES ARGUMENTOS ENTRE PARENTESIS, DIVIDIDO CON UN PARENTESIS
+print("primer argumento", "segundo argumento",sep="_")# sep ="_"   = me daja un guion bajo en el medio de los dos argumentos
 print("primer argumento", "segundo argumento",end="\n")# end="\n" = me deja un espacio entre los dos argumentos
 print ("R","O","M", "Y ",end=" * ")
-#print ("la funcion\"print\"...")"\n"
+#print ("la funcion\"print\"...")"\n"  #SI ESTA FUERA DE LOS PARENTESIS DARÁ ERROR
 print ('la funcion "print"...'"\t")# si comienza con comilla ''simple, termina con las mismas
 print ('la funcion print\t hola')# si comienza con comilla dobre, termina con comilla doble
 #claer = para limpiar
--- entrega/test.py
+++ entrega/test.py
@@ -2,5 +2,26 @@ print ("LAS MAQUINAS ME SORPRENDEN CON MUCHA FRECUENCIA")
 print ()
 print("hoy es viernes :)")
 print ("23", '"23"')
-print ('una computadora puede ser llamada "inteligente" si logra engañar a una personahacie{endole} haciéndole creer que
-es un humano.')
+print ('una computadora puede ser llamada "inteligente" si logra engañar a una persona haciendole creer que es un humano.')
+print ("ROMINA","BARROS", 36)
+print ("ROMINA","BARROS", 36,sep="_") #"G"
+print ("gregores", 988, 9400,"\t")#H
+print ("gregores", 988, 9400, end="\n")#hace para que sea en otra linea????
+print ("Feliz", "Primavera", 2023, "\n")
+print ("Feliz", "Primavera", 2023, "\n")# permite imprimir en forma de escalera Feliz Primavera 2023
+print ("              'solo podemos ver poco del futuro'  ")#
+print ("pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+print ("    *")
+print ("   * *")
+print ("  *   *")
+print (" *     *")
+print ("***   ***")
+print ("  *   *")
+print ("  *   *")
+print ("  *****")
+print (5+7) #suma
+print (0-9) #resta
+print (2+45)
+print (3/2) #division 
+print (6**2) #potencia
+print (3//2) # division que solo da el número entero del resultado "//"
