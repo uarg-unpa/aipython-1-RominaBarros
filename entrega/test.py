d8142dea44a35485fcf741e5fede610c91ecbaa6
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-23 01:53:44 +0000

    1

--- entrega/test.py
+++ entrega/test.py
@@ -2,11 +2,11 @@ print ("LAS MAQUINAS ME SORPRENDEN CON MUCHA FRECUENCIA")
 print ()
 print("hoy es viernes :)")
 print ("23", '"23"')
-print ('una computadora puede ser llamada "inteligente" si logra engañar a una persona haciendole creer que es un humano.')
-print ("ROMINA","BARROS", 36)
+print ('una computadora puede ser llamada "inteligente" si logra engañar a una persona haciendole creer que es un humano.')#"E"
+print ("ROMINA","BARROS", 36) #F- entre los argumentos se pone coma y da como resultado un espacio
 print ("ROMINA","BARROS", 36,sep="_") #"G"
 print ("gregores", 988, 9400,"\t")#H
-print ("gregores", 988, 9400, end="\n")#hace para que sea en otra linea????
+print ("gregores", 988, 9400, end="\n")# i - hace para que sea en otra linea????
 print ("Feliz", "Primavera", 2023, "\n")
 print ("Feliz", "Primavera", 2023, "\n")# permite imprimir en forma de escalera Feliz Primavera 2023
 print ("              'solo podemos ver poco del futuro'  ")#
