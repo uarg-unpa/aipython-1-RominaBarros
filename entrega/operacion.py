8da1bd0302588510fa5b7a4ff5fcee68c80effe7
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-23 19:02:59 +0000

    operacion

--- entrega/operacion.py
+++ entrega/operacion.py
@@ -1,4 +1,7 @@
-print(22+33)# suma
-print(22-33)# resta
+print(22+33)# suma                      unarios
+print(22-33)# resta                     unarios
+print(22**33)# potencia
 print(22*33)# multiplicacion
 print(22/33)# division
+print(22%33)# modulo
+print(22//33)# division entera
