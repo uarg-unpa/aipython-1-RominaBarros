1d67b3ccd1be4540a8c8ac05a5d161e7136764c7
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-10-23 18:58:39 +0000

    operacion.py

---
+++ entrega/operacion.py
@@ -0,0 +1,4 @@
+print(22+33)# suma
+print(22-33)# resta
+print(22*33)# multiplicacion
+print(22/33)# division
--- entrega/test.py
+++ entrega/test.py
@@ -8,7 +8,7 @@ print ("ROMINA","BARROS", 36,sep="_") #"G"
 print ("gregores", 988, 9400,"\t")#H
 print ("gregores", 988, 9400, end="\n")# i - hace para que sea en otra linea????
 print ("Feliz", "Primavera", 2023, "\n")
-print ("Feliz", "Primavera", 2023, "\n")# permite imprimir en forma de escalera Feliz Primavera 2023
+print ("Feliz" ,"\n", "Primavera","\n",   2023, "\n")# permite imprimir en forma de escalera Feliz Primavera 2023
 print ("              'solo podemos ver poco del futuro'  ")#
 print ("pero lo suficiente para darnos cuenta de que hay mucho que hacer")
 print ("    *")
@@ -24,4 +24,4 @@ print (0-9) #resta
 print (2+45)
 print (3/2) #division 
 print (6**2) #potencia
-print (3//2) # division que solo da el número entero del resultado "//"
+print (3//2) # division que solo muestra el número entero del resultado "//"
