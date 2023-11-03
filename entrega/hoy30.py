42ab799fe9e74438d1a1287f687f9de5d86266e2
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-03 17:49:19 +0000

    1

--- entrega/hoy30.py
+++ entrega/hoy30.py
@@ -1,11 +1,11 @@
-str="bienvenidos a AIPython"
-print(f"cadena original{str}")
+str1=" bienvenidos a AIPython "
+print(f"cadena original{str1}")
 str=str1.upper()
-print(f"cadena despues de usar la funcion upper{str1}")
-str1=str1.lower{}
-print(f"cadena despues de usar la cadena lower{str1}")
-str1=str1.litle{}
-print(f"cadena despues de usar la funcion Title{str1}")
+print(f"cadena despues de usar la funcion upper {str1}")
+str1=str1.lower()
+print(f"cadena despues de usar la cadena lower {str1}")
+str1=str1.litle()
+print(f"cadena despues de usar la funcion Title {str1}")
 print(f"usando * en cadena {str1*3}")
 print(f"la suma de los numeros es {2+4}")
 #REBANADAS
