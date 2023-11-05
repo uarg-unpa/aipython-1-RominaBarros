d87cc0f5b72125027f838103ddce83e1b8da3387
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-05 00:57:04 +0000

    .

--- entrega/tpN°1.py
+++ entrega/tpN°1.py
@@ -1,4 +1,4 @@
-cel=input("ingrese la temperatura en grados celcius:")
-cel=float(cel)
+costohs=input("ingrese el costo de la horas:")
+costohs=float(costohs)
 far=(cel * 9/5)+32      #(0 °C × 9/5) + 32 = 32 °F
-print("la temperarura ingresada ",cel," celcius son: ",far,"fahrenheit")
+print("la temperaruhsra ingresada ",cel," celcius son: ",far,"fahrenheit")
