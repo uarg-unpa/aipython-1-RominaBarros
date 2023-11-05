155540a224e047b0a110918e332e9efdcf635a68
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-05 02:49:33 +0000

    12

--- entrega/test.py
+++ entrega/test.py
@@ -149,3 +149,14 @@ costohs=float(costohs)
 
 pago=(hs * costohs)      
 print("por ",hs,"horas trabajadas a $",costohs,"pesos la hora, recibira un pago de:$",pago,"pesos")     
+
+#12
+dinero=input("ingrese la cantidad de dinero que quiere invertir: $")
+dinero=float(dinero)
+interes=input("ingrese el interes anual:")
+interes=int(interes)
+años=input("ingrese la cantidad de años que desea invertir:")
+años=int(años)
+
+capital=(dinero/100)*(1+(interes)**años)      
+print("el capital obtenido fue de $ ",capital,"pesos, de un monto invertido de ",dinero,"pesos, con una taza de interes de:",interes,"a ",años, "años")
--- entrega/tpN°1.py
+++ entrega/tpN°1.py
@@ -1,12 +1,9 @@
 dinero=input("ingrese la cantidad de dinero que quiere invertir: $")
 dinero=float(dinero)
-interes=input("ingrese el costo del interes anual:")
-interes=float(interes)
+interes=input("ingrese el interes anual:")
+interes=int(interes)
 años=input("ingrese la cantidad de años que desea invertir:")
 años=int(años)
 
-capital=dinero(1+interes*años)      
+capital=(dinero/100)*(1+(interes)**años)      
 print("el capital obtenido fue de $ ",capital,"pesos, de un monto invertido de ",dinero,"pesos, con una taza de interes de:",interes,"a ",años, "años")
-M = C*(1 + i*n), donde “C” es el capital inicial, “i” es la tasa de interés aplicable a cada período y “n” es el número de períodos en que se generarán los intereses.
-Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual
-y el número de años, y muestre por pantalla el capital obtenido en la inversión
