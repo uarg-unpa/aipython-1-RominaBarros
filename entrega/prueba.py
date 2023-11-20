a3dfb3b720da13df15e52e7c3b12e764fffbe417
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-20 15:20:35 +0000

    ,

--- entrega/prueba.py
+++ entrega/prueba.py
@@ -2,15 +2,17 @@
 #   (“Ingrese su edad:”)Use un condición anidada para: Imprimir año si la diferencia es de 1, sino años para 
 #   diferencias mayores.Cuando las edades son iguales imprimir un mensaje personalizado,ser creativo!!
 cont=0
-i=36
+i=0
 edad=int(input("Mi edad es 36,para comparar, ingrese su edad: "))
+if edad>36:
+    i=edad-36
+    print("usted es mayor que yo")
+ #   for i in  edad():
+    #    cont=cont+1
+    print("tenemos ",i, "años de diferencia")
+else:
+    i=36-edad
+    print("soy mayor que vos")
 if edad==36:
     print("tenemos la misma edad, que copado. conocimos la vida sin telefono celular. y fuimos felices jugando afuera con los amigos y vecinos")
-if edad>36:
-    print("usted es mayor que yo")
-    for i in  edad():
-        cont=cont+1
-        print("tenemos ",cont, "años de diferencia")
-else:
-    print("soy mayor que vos")
-    ("para conducir, le faltan",18-edad,"años")
+    
