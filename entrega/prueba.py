66db5aa5c076a9871d5d479b3b7181a672e70c2b
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-20 02:26:01 +0000

    2

--- entrega/prueba.py
+++ entrega/prueba.py
@@ -1,2 +1,16 @@
-# 20. Dibujar dos árboles uno al lado del otro.
-print ("        *","                *","\n","     *    *","          *    *","\n","   *        *","      *        *","\n"," *            *","  *            *","\n","***          ***","***          ***","\n","  *          *","    *          *","\n","  *          *","    *          *","\n","  ************","    ************")
+#2. Compare su edad y mi edad usando if..else. ¿Quién es mayor (vos o yo)?, para el ingreso de la edad use input
+#   (“Ingrese su edad:”)Use un condición anidada para: Imprimir año si la diferencia es de 1, sino años para 
+#   diferencias mayores.Cuando las edades son iguales imprimir un mensaje personalizado,ser creativo!!
+cont=0
+i=36
+edad=int(input("Mi edad es 36,para comparar, ingrese su edad: "))
+if edad==36:
+    print("tenemos la misma edad, que copado. conocimos la vida sin telefono celular. y fuimos felices jugando afuera con los amigos y vecinos")
+if edad>36:
+    print("usted es mayor que yo")
+    for i in  edad():
+        cont=cont+1
+        print("tenemos ",cont, "años de diferencia")
+else:
+    print("soy mayor que vos")
+    ("para conducir, le faltan",18-edad,"años")
