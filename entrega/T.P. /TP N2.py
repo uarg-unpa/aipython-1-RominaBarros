7c499687f7140f801134d0ff6eb7285d476e9ec2
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-20 18:37:50 +0000

    2.2

--- entrega/T.P. /TP N2.py
+++ entrega/T.P. /TP N2.py
@@ -6,3 +6,22 @@ if edad>18:
     print("tiene edad suficiente para conducir")
 else:
     print("para conducir, le faltan",18-edad,"años")    
+
+#2. Compare su edad y mi edad usando if..else. ¿Quién es mayor (vos o yo)?, para el ingreso de la edad use input
+#   (“Ingrese su edad:”)Use un condición anidada para: Imprimir año si la diferencia es de 1, sino años para 
+#   diferencias mayores.Cuando las edades son iguales imprimir un mensaje personalizado,ser creativo!!
+
+edad=int(input("Mi edad es 36,para comparar, ingrese su edad: "))
+if edad==36: 
+    print("lo mas lindo, que conocimos la vida sin telefono celular. y fuimos felices jugando afuera con los amigos y vecinos")
+else:
+    if edad>36:
+        print("usted es mayor que yo")
+        i=edad-36
+    else:
+        i=36-edad
+        print("soy mayor que vos")
+if i ==1:
+    print("tenemos ",i, "año de diferencia")
+else:
+    print("tenemos ",i, "años de diferencia")    
--- entrega/prueba.py
+++ entrega/prueba.py
@@ -1,18 +1,18 @@
 #2. Compare su edad y mi edad usando if..else. ¿Quién es mayor (vos o yo)?, para el ingreso de la edad use input
 #   (“Ingrese su edad:”)Use un condición anidada para: Imprimir año si la diferencia es de 1, sino años para 
 #   diferencias mayores.Cuando las edades son iguales imprimir un mensaje personalizado,ser creativo!!
-cont=0
-i=0
+
 edad=int(input("Mi edad es 36,para comparar, ingrese su edad: "))
-if edad>36:
-    i=edad-36
-    print("usted es mayor que yo")
- #   for i in  edad():
-    #    cont=cont+1
-    print("tenemos ",i, "años de diferencia")
+if edad==36: 
+    print("lo mas lindo, que conocimos la vida sin telefono celular. y fuimos felices jugando afuera con los amigos y vecinos")
 else:
-    i=36-edad
-    print("soy mayor que vos")
-if edad==36:
-    print("tenemos la misma edad, que copado. conocimos la vida sin telefono celular. y fuimos felices jugando afuera con los amigos y vecinos")
-    
+    if edad>36:
+        print("usted es mayor que yo")
+        i=edad-36
+    else:
+        i=36-edad
+        print("soy mayor que vos")
+if i ==1:
+    print("tenemos ",i, "año de diferencia")
+else:
+    print("tenemos ",i, "años de diferencia")    
