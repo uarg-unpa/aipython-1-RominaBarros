c5768ffeb2af71ea15910525a5d598d3cd195535
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-25 12:51:31 +0000

    7

--- entrega/T.P. /TP N2.py
+++ entrega/T.P. /TP N2.py
@@ -81,3 +81,22 @@ if dia>0 and dia<8:
 else:
     print("el numero ingresado no corresponde a un día de la semana")  
 
+#7. Genere un programa que clasifique a los estudiantes según sus puntuaciones:
+#● 90-100, A;● 70-89, B; ● 60-69, C; ● 50-59, D; ● 0-49, F
+
+nota =int(input("ingrese la puntuacion del alumno: "))
+if nota>0 and nota<101:
+    if nota>89:
+        print("la puntuación obtenida es:'A' ")
+    else:
+        if nota>69:
+            print("la puntuación obtenida es:'B' ")
+        else:
+            if nota >59:
+                print("la puntuación obtenida es:'C' ")
+            else:
+                if nota>49:
+                    print("la puntuación obtenida es:'D' ")
+                else:
+                    print("la puntuación obtenida es:'F' ")    
+
--- entrega/prueba.py
+++ entrega/prueba.py
@@ -1,18 +1,18 @@
 #7. Genere un programa que clasifique a los estudiantes según sus puntuaciones:
-#● 80-100, A;● 70-89, B; ● 60-69, C; ● 50-59, D; ● 0-49, F
+#● 90-100, A;● 70-89, B; ● 60-69, C; ● 50-59, D; ● 0-49, F
 
 nota =int(input("ingrese la puntuacion del alumno: "))
-while nota>0 and nota<101:
-    if nota <50:
-	    print("la puntuación obtenida es F ")
-else:
-    if nota <60:
-        print("la puntuación obtenida es D ")
+if nota>0 and nota<101:
+    if nota>89:
+        print("la puntuación obtenida es:'A' ")
     else:
-	    if nota<70:
-	        print("la puntuación obtenida es C ")
+        if nota>69:
+            print("la puntuación obtenida es:'B' ")
         else:
-            if nota <80:
-               	print("la puntuación obtenida es B ")
-           	else:
-		        print("la puntuación obtenida es A ")
+            if nota >59:
+                print("la puntuación obtenida es:'C' ")
+            else:
+                if nota>49:
+                    print("la puntuación obtenida es:'D' ")
+                else:
+                    print("la puntuación obtenida es:'F' ")   
