2028dd1a9e8c9f19a322b233d08f6dc45248a6b2
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-25 13:11:50 +0000

    9

--- entrega/T.P. /TP N2.py
+++ entrega/T.P. /TP N2.py
@@ -98,5 +98,34 @@ if nota>0 and nota<101:
                 if nota>49:
                     print("la puntuación obtenida es:'D' ")
                 else:
-                    print("la puntuación obtenida es:'F' ")    
+                    print("la puntuación obtenida es:'F' ")
+
+#8. Escriba un programa para una empresa que tiene salas de juegos para todas las
+#edades y quiere calcular de forma automática el precio que debe cobrar a sus
+#clientes por entrar. El programa debe preguntar al usuario la edad del cliente y
+#mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis,
+#si tiene entre 4 y 18 años debe pagar $5 y si es mayor de 18 años, $10.
+
+edad=int(input("ingrese su edad: "))
+if edad>0 and edad<150:
+      if edad >18:
+          print("el precio de la entrada es: $10 ")
+      else:
+        if edad >4:
+            print("el precio de la entrada es: $5 ")
+        else:
+            print("entra gratis ")   
+
+#9. Para pagar un determinado impuesto se debe ser mayor de 18 años y tener ingresos
+#iguales o superiores a $100000 mensuales. Escribir un programa que pregunte al
+#usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene
+#que pagar o no el impuesto.
+
+edad=int(input("ingrese su edad: "))
+ingreso=int(input("ingrese su sueldo: "))
+if edad>18:
+    if ingreso>=100000:
+	    print(" debe pagar el impuesto ")
+else:
+      print(" no debe pagar el impuesto ")  
 
