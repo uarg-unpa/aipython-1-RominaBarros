fbc215f1c4a4c8c264ae1934407f5141829ee53e
RominaBarros <147439959+RominaBarros@users.noreply.github.com>
2023-11-29 22:38:16 +0000

    ALGUNOS

--- entrega/T.P. /TP N°4.py
+++ entrega/T.P. /TP N°4.py
@@ -16,7 +16,6 @@ for i in range(len(numeros)):               #           //esta forma te da los n
 
 #4. Crear una lista de tus frutas favoritas
 #a. Imprimir la longitud.
-
 lista_frutas=["manzana","banana","frutilla","naranja","durazno"]
 print(lista_frutas)                                               #muestra la lista una fruta abajo de la otra  
 for i in range(len(lista_frutas)):#                    //mustra una letra abajo del otra, de todas las palabras
@@ -24,6 +23,35 @@ for i in range(len(lista_frutas)):#                    //mustra una letra abajo 
         print(lista_frutas[i][j])
 
 #b. Eliminar el primer elemento de la lista.
-
 lista_frutas.remove("manzana")
 print(lista_frutas)                
+
+#c. Agregar un elemento al final de la lista.
+lista_frutas.append("pelón")
+print(lista_frutas)
+
+
+
+
+
+
+
+
+
+#11. Crear una lista de números del 1 al 10.
+numeros=[1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+#a. Imprimir los tres primeros números utilizando rebanadas.
+
+
+#12. Crear una lista de letras (‘a’ hasta ‘j’)
+letras=["a","b","c","d","e","f","g","h","i","j"]
+print(letras)
+
+
+
+
+#15. Crear la siguiente lista =>flores= [rosas, orquídea,lirio,tulipan, margarita, dalia, hortensia]
+flores= ["rosas","orquídea","lirio","tulipan", "margarita", "dalia", "hortensia"]
+print(flores)
--- entrega/prueba.py
+++ entrega/prueba.py
@@ -1,13 +1,3 @@
-#4. Crear una lista de tus frutas favoritas
-#a. Imprimir la longitud.
-
-lista_frutas=["manzana","banana","frutilla","naranja","durazno"]
-print(lista_frutas)                                               #muestra la lista una fruta abajo de la otra  
-for i in range(len(lista_frutas)):#                    //mustra una letra abajo del otra, de todas las palabras
-    for j in range(len(lista_frutas)):
-        print(lista_frutas[i][j])
-
-#b. Eliminar el primer elemento de la lista.
-
-lista_frutas.remove("manzana")
-print(lista_frutas)
+#15. Crear la siguiente lista =>flores= [rosas, orquídea,lirio,tulipan, margarita, dalia, hortensia]
+flores= ["rosas","orquídea","lirio","tulipan", "margarita", "dalia", "hortensia"]
+print(flores)
